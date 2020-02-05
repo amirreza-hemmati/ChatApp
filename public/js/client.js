aa0107eb5f7cb572ef6a1602f4c626b11803d5eb
amir <dev.amirhemmati@yahoo.com>
2020-02-05 20:17:31 +0330

    rename the file => client.js with socket.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const socket = io();
+
+socket.on("connect", () => {
+  console.log("user connected to the messenger");
+});
+
+socket.on("disconnect", () => {
+  console.log("user disconnect to the messenger");
+});
+
+// on =>
+/*
+  connection => وقتی در سرور سوکتی دریافت شود این متد اجرا میشود و دارای یک پارامتر است
+  connect => وقتی در فرانت کاربری با سرور کانکت شد اجرا میشود
+*/
