169f7d97dba0cf449c3282de5d4954b5ad851f96
amir <dev.amirhemmati@yahoo.com>
2020-02-05 20:18:05 +0330

    build a connection and desconnect with socket.io

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const socketIo = require("socket.io");
-const mongoose = require("mongoose");
-const User = require("./models/users");
 
 app.set("views", "views");
 app.set("view engine", "ejs");
@@ -18,64 +16,39 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// هروقت کاربری کانکت شد
-io.on("connection", socket => {
-  socket.on("new user", informationUser => {
-    const { userName, email } = informationUser;
 
-    const newUser = new User({
-      userName,
-      email
-    });
 
-    newUser
-      .save()
-      .then(user => {
-        socket.userName = user.userName;
-        socket._id = user._id;
-        io.emit("join", {
-          success: true,
-          message: "کاربر با موفقیت به چت پیوست",
-          userName: socket.userName
-        });
-      })
-      .catch(error => {
-        io.emit("join", {
-          success: false,
-          code: error.code,
-          error
-        });
-      });
-  });
+
+
+
+// connection method
+io.on('connection', socket => {
+  
+  console.log("a new user just connected");
 
   socket.on("disconnect", () => {
-    User.findByIdAndDelete(socket._id)
-      .then(() => {
-        io.emit("left user", {
-          success: true,
-          message: "کاربر با موفقیت حذف گردید",
-          userName: socket.userName
-        });
-      })
-      .catch(() => {
-        io.emit("left user", {
-          success: false,
-          message: "کاربر با موفقیت حذف نگردید ..."
-        });
-      });
+    console.log("user disconnect to the messenger");
   });
+
 });
 
-const configMongo = { useUnifiedTopology: true, useNewUrlParser: true };
 
-mongoose
-  .connect(process.env.URL, configMongo)
-  .then(() => {
-    console.log("Connect to mongodb database is => ", true);
-    server.listen(process.env.PORT, () => {
-      console.log("Starting the server is =>", true);
-    });
-  })
-  .catch(error => {
-    console.log("Error in connect to mongodb database err=>", error);
-  });
+
+
+
+
+
+server.listen(process.env.PORT, () => {
+  console.log("Starting the server is =>", true);
+});
+
+// const configMongo = { useUnifiedTopology: true, useNewUrlParser: true };
+// mongoose
+//   .connect(process.env.URL, configMongo)
+//   .then(() => {
+//     console.log("Connect to mongodb database is => ", true);
+            //server listen
+//   })
+//   .catch(error => {
+//     console.log("Error in connect to mongodb database err=>", error);
+//   });
